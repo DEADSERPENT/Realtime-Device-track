21b425a77bdc5c46d31ed23ea2ba11343f9897e4
M R SHIVAKUMAR <137382026+Bluie0@users.noreply.github.com>
2025-07-12 18:09:51 +0530

    ES6 implemented

---
+++ server/app.js
@@ -0,0 +1,81 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const http = require('http');
+// const socketio = require('socket.io');
+// const dotenv = require('dotenv');
+
+// const PORT = process.env.PORT || 3000;
+
+// const server = http.createServer(app);
+// const io = socketio(server);
+
+// app.set("view engine", "ejs");
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// io.on("connection", function (socket) {
+//     console.log("User connected:", socket.id);
+    
+//     socket.on("send-location", function (data) {
+//         io.emit("receive-location", { id: socket.id, ...data });
+//     });
+    
+//     socket.on("disconnect", function () {
+//         console.log("User disconnected:", socket.id);
+//         io.emit("user-disconnected", socket.id);
+//     });
+// });
+
+// app.get('/', function (req, res) {
+//     res.render('index');
+// });
+
+// server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+import express from 'express';
+import path from 'path';
+import http from 'http';
+import { Server as socketio } from 'socket.io';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import pkg from "cors";
+import cookieParser from 'cookie-parser';
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(pkg());
+app.use(cookieParser({Credential:true}));
+
+// Required to replicate __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const server = http.createServer(app);
+const io = new socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on("connection", function (socket) {
+    console.log("User connected:", socket.id);
+    
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
+    
+    socket.on("disconnect", function () {
+        console.log("User disconnected:", socket.id);
+        io.emit("user-disconnected", socket.id);
+    });
+});
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
